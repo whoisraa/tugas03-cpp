07d072e98b3fd8c32b5930b6454a8966feaf3da7
whoisraa <rairaiatlantica@gmail.com>
2021-06-26 14:51:28 +0700

    tugas 3 cpp

---
+++ tugas03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int A,B;
+	A = 10;
+	B = 20;
+	cout<<"Nilai Variabel A : ";
+	cout<<A<<endl;
+	cout<<"Nilai Variabel B : ";
+	cout<<B<<endl;
+	cout<<"Hasil Perkalian Kedua Variabel Adalah : ";
+	cout<<A*B;
+}

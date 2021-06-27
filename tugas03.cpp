aa1c96544747dabeaafb31cfeb244b26baa423db
whoisraa <rairaiatlantica@gmail.com>
2021-06-27 13:53:04 +0700

    tugas 3 cpp

--- tugas03.cpp
+++ tugas03.cpp
@@ -4,10 +4,7 @@ int main(){
 	int A,B;
 	A = 10;
 	B = 20;
-	cout<<"Nilai Variabel A : ";
-	cout<<A<<endl;
-	cout<<"Nilai Variabel B : ";
-	cout<<B<<endl;
-	cout<<"Hasil Perkalian Kedua Variabel Adalah : ";
-	cout<<A*B;
+	cout<<"Nilai Variabel A : "<<A<<endl;
+	cout<<"Nilai Variabel B : "<<B<<endl;
+	cout<<"Hasil Perkalian Kedua Variabel Adalah : "<<A*B;
 }
